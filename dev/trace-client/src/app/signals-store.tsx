f9a531e1fffa6d3e3d4012e283482f47c3abf8a8
Sushant Agrawal <capitalch@gmail.com>
2023-10-27 18:34:42 +0530

    init

---
+++ dev/trace-client/src/app/signals-store.tsx
@@ -0,0 +1,27 @@
+import { Signal, signal } from '@preact/signals-react'
+import _ from 'lodash'
+
+const SignalsStoreT: SignalsStoreType = {
+    main: {
+        hideSideBarClicked: false,
+        showSidBarClicked: false,
+        login: {
+            isLoggedIn: signal(false),
+        }
+    },
+}
+
+const SignalsStore: SignalsStoreType = _.cloneDeep(SignalsStoreT)
+
+type SignalsStoreType = {
+    main: {
+        hideSideBarClicked: boolean
+        showSidBarClicked: boolean
+        login: {
+            isLoggedIn: Signal<boolean>
+        }
+    },
+
+}
+
+export { SignalsStore }
