603abf72afa7e3910ce5236451adf46b013f3835
Sushant Agrawal <capitalch@gmail.com>
2024-01-20 20:54:23 +0530

    init

--- lab/react/vite-lab1/src/features/array-trick/array-trick.tsx
+++ lab/react/vite-lab1/src/features/array-trick/array-trick.tsx
@@ -1,6 +1,8 @@
-function ArrayTrick(){
+import _ from "lodash"
+
+function ArrayTrick() {
     // const nestedArray = ['address1', 'address2', 'pin', ['status']]
-    const nestedObject = {
+    const nestedObject: any = {
         address: '12 J.L',
         pin: '700013',
         status: {
@@ -8,26 +10,36 @@ function ArrayTrick(){
         },
 
     }
-    return(<div className="m-2">
+    return (<div className="m-2">
         <button className="px-2 bg-slate-200" onClick={handleClick}>Do Trick</button>
     </div>)
 
-    function handleClick(){
-
+    function handleClick() {
+        const value1 = _.get(nestedObject,'status.name')
+        const value = getValue('status.name')
+        alert(value1)
+        console.log(value)
     }
 
-    function getValue(item: string){
-        if(item.includes('.')){
+    function getValue(item: string) {
+        let ret: any = ''
+        if (item.includes('.')) {
             const itemArray = item.split('.')
+            ret = getIteratedValue(itemArray)
 
+        } else {
+            ret = nestedObject[item]
         }
+        return (ret)
     }
 
-    function doIterate(itemArray: string[]){
-        itemArray.forEach((x:string)=>{
-            
+    function getIteratedValue(itemArray: string[]) {
+        let obj: any = _.cloneDeep(nestedObject)
+        itemArray.forEach((x: string) => {
+            obj  = obj[x]
         })
+        return(obj)
     }
 }
 
-export {ArrayTrick}
+export { ArrayTrick }
