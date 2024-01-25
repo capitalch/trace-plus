abac9176bcf3a45b841a1e0f7f9310e3ccaaac3b
capitalch <capitalch@gmail.com>
2024-01-25 12:49:03 +0530

    init

--- lab/react/vite-lab1/src/features/navigation/layouts/layouts.tsx
+++ lab/react/vite-lab1/src/features/navigation/layouts/layouts.tsx
@@ -1,7 +1,5 @@
-import { useContext, useEffect } from "react"
+import { useContext} from "react"
 import { Sidebar } from "../sidebar/sidebar"
-import { SignalsStore } from "../../../app/signals-store"
-// import { useDispatch } from "react-redux"
 import { setSidebarOpen } from "../navigation-slice"
 import { AppDispatch, store } from "../../../app/store"
 import { GlobalContext } from "../../../App"
@@ -11,12 +9,7 @@ function Layouts() {
     const dispatch:AppDispatch = useAppDispatch()
     const { profile }: any = useContext(GlobalContext)
 
-    useEffect(() => {
-        // SignalsStore.layouts.isSideBarOpen.value = true
-    })
-
-    return (<div className="flex">
-        <span>{SignalsStore.layouts.isSideBarOpen.value}</span>
+    return (<div className="flex">        
         <Sidebar />
         <button onClick={handleSidebarToggle} className="px-2 bg-slate-200 h-8 rounded-md">Toggle sidebar</button>
         <button onClick={handleOnChangeContext}>Change Context Value</button>
@@ -24,9 +17,7 @@ function Layouts() {
 
     function handleSidebarToggle() {
         const state = store.getState();
-        // const args: any = { isSidebarOpen: !(state?.navigation?.isSidebarOpen || false) }
         dispatch(setSidebarOpen({isSidebarOpen: !(state?.navigation?.isSidebarOpen || false)}))
-        // SignalsStore.layouts.isSideBarOpen.value =  !(SignalsStore.layouts.isSideBarOpen.value || false)
     }
 
     function handleOnChangeContext() {
