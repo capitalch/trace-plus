cd5d2faf39df16c447f7da465b40460e79b81205
capitalch <capitalch@gmail.com>
2024-06-07 00:46:16 +0530

    init

--- dev/trace-client/src/controls/widgets/widget-button-refresh.tsx
+++ dev/trace-client/src/controls/widgets/widget-button-refresh.tsx
@@ -2,14 +2,14 @@ import { IconIconRefresh } from "../icons/icon-icon-refresh";
 
 export function WidgetButtonRefresh({ handleRefresh }: WidgetButtonRefreshType) {
     return (
-        <button className="h-12 rounded-3xl bg-slate-100 hover:bg-slate-200 px-2" onClick={handleRefresh}>
+        <button className="h-12 rounded-xl bg-slate-50 hover:bg-slate-100 px-2" onClick={handleRefresh}>
             <IconIconRefresh className='h-10 w-10 text-primary-400 hover:text-primary-600' />
         </button>)
 }
 
 type WidgetButtonRefreshType = {
-    handleRefresh: (args?: any) => void
+    handleRefresh: (args?: ArgsType) => void
 }
-// type ArgsType = {
-//     [item: string]: string | undefined
-// }cd 
+type ArgsType = {
+    [item: string]: string | undefined | any
+}
