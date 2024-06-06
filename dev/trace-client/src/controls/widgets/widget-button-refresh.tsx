9ac498b04854c7eeac7e08bc2c3567ab94ed6a20
Sushant Agrawal <capitalch@gmail.com>
2024-06-06 18:56:52 +0530

    init

--- dev/trace-client/src/controls/widgets/widget-button-refresh.tsx
+++ dev/trace-client/src/controls/widgets/widget-button-refresh.tsx
@@ -2,8 +2,8 @@ import { IconIconRefresh } from "../icons/icon-icon-refresh";
 
 export function WidgetButtonRefresh({ handleRefresh }: WidgetButtonRefreshType) {
     return (
-        <button className="h-12 rounded-3xl bg-primary-400 px-2 hover:bg-primary-600" onClick={handleRefresh}>
-            <IconIconRefresh className='h-9 w-9 text-gray-300 hover:text-white' />
+        <button className="h-12 rounded-3xl bg-slate-100 hover:bg-slate-200 px-2" onClick={handleRefresh}>
+            <IconIconRefresh className='h-10 w-10 text-primary-400 hover:text-primary-600' />
         </button>)
 }
 
@@ -12,4 +12,4 @@ type WidgetButtonRefreshType = {
 }
 // type ArgsType = {
 //     [item: string]: string | undefined
-// }
+// }cd 
