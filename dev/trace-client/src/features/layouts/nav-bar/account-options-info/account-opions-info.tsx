26a5aca2ad6df0e0c4caa4c45a5a66e7373eaf7b
Sushant Agrawal <capitalch@gmail.com>
2024-11-14 21:32:29 +0530

    init

--- dev/trace-client/src/features/layouts/nav-bar/account-options-info/account-opions-info.tsx
+++ dev/trace-client/src/features/layouts/nav-bar/account-options-info/account-opions-info.tsx
@@ -1,16 +1,49 @@
+import { IconMinusCircle } from "../../../../controls/icons/icon-minus-circle";
+import { IconPlusCircle } from "../../../../controls/icons/icon-plus-circle";
 
 
 export function AccountOptionsInfo() {
-    return (<div className="flex items-center ml-20">
-        <button className="w-60 h-8 flex items-center px-2 py-2 text-gray-800 bg-gray-200 rounded-full shadow">
-            {/* Badge section */}
-            <div className="px-1 py-1 text-xs font-bold text-white bg-blue-500 rounded-full">
-                BU
+    return (
+        <div className="ml-8 flex items-center bg-gray-500 rounded-full px-2 py-2">
+            {/* Business unit */}
+            <button className="flex h-8 w-60 items-center rounded-full bg-gray-200 px-2 py-2 text-gray-800 shadow">
+                {/* Badge section */}
+                <div className="rounded-full bg-blue-500 px-1 py-1 text-xs font-bold text-white">
+                    BU
+                </div>
+                {/* Text section */}
+                <span className="ml-1 overflow-hidden text-ellipsis whitespace-nowrap text-sm font-medium">Business  unit 1</span>
+            </button>
+
+            {/* financial year */}
+            <div className="ml-4 flex items-center">
+                {/* Plus */}
+                <button>
+                    <IconPlusCircle className="h-7 w-7" />
+                </button>
+                {/* Financial year */}
+                <button className="w-70 ml-1 flex h-8 items-center rounded-full bg-gray-200 px-2 py-2 text-gray-800 shadow">
+                    {/* Badge section */}
+                    <div className="rounded-full bg-blue-500 px-1 py-1 text-xs font-bold text-white">
+                        FY
+                    </div>
+                    {/* Text section */}
+                    <span className="ml-1 overflow-hidden text-ellipsis whitespace-nowrap text-sm font-medium">2024 (01/04/2024 - 31/03/2025)</span>
+                </button>
+                {/* minus */}
+                <button className="ml-1">
+                    <IconMinusCircle className="h-7 w-7" />
+                </button>
             </div>
-            {/* Text section */}
-            <span className="overflow-hidden text-xs font-medium text-ellipsis whitespace-nowrap">Business  unit 1</span>
-        </button>
-        {/* <span>Fy</span>
-        <span>Branch</span> */}
-    </div>)
+
+            {/* Branch */}
+            <button className="w-70 ml-4 flex h-8 items-center rounded-full bg-gray-200 px-2 py-2 text-gray-800 shadow">
+                {/* Badge section */}
+                <div className="rounded-full bg-blue-500 px-1 py-1 text-xs font-bold text-white">
+                    BR
+                </div>
+                {/* Text section */}
+                <span className="ml-1 overflow-hidden text-ellipsis whitespace-nowrap text-sm font-medium">Head office</span>
+            </button>
+        </div>)
 }
